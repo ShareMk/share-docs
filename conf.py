99d0bf3103d6ed46877b8dfe98a2b463d13a2153
ShareMk <33144264+ShareMk@users.noreply.github.com>
2025-03-20 18:41:37 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,115 @@
-project = 'shre-docs'
-copyright = '2025, ShareMk'
-author = 'ShareMk'
-extensions = ['sphinx.ext.autodoc']
-html_theme = 'sphinx_rtd_theme'  # 使用 Read the Docs 主题
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'My Project'
+copyright = '2023, Your Name'
+author = 'Your Name'
+
+# The short X.Y version
+version = '1.0'
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'myst_parser',
+]
+
+# 支持的文件扩展名
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = 'en'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Options for HTMLHelp output ---------------------------------------------
+
+htmlhelp_basename = 'MyProjectdoc'
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+    'preamble': '',
+    'figure_align': 'htbp',
+}
+
+latex_documents = [
+    (master_doc, 'MyProject.tex', 'My Project Documentation',
+     'Your Name', 'manual'),
+]
+
+# -- Options for manual page output ------------------------------------------
+
+man_pages = [
+    (master_doc, 'myproject', 'My Project Documentation',
+     [author], 1)
+]
+
+# -- Options for Texinfo output ----------------------------------------------
+
+texinfo_documents = [
+    (master_doc, 'MyProject', 'My Project Documentation',
+     author, 'MyProject', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+# -- Options for Epub output -------------------------------------------------
+
+epub_title = project
+epub_exclude_files = ['search.html']
+
+
+# 启用章节编号
+numfig = True
+numfig_format = {
+    'section': 'Section %s',
+    'figure': 'Figure %s',
+    'table': 'Table %s',
+    'code-block': 'Listing %s',
+}
+
+# 自动为章节编号
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '10pt',
+    'preamble': r'''
+        \usepackage{charter}
+        \usepackage[defaultsans]{lato}
+        \usepackage{inconsolata}
+    ''',
+}
+
+# 启用章节编号
+html_theme_options = {
+    'number_sections': True,
+}
+
+# GitHub 仓库信息
+html_context = {
+    "display_github": True,  # 启用 GitHub 链接
+    "github_user": "ShareMk",  # GitHub 用户名
+    "github_repo": "share-docs",  # 仓库名称
+    "github_version": "main",  # 分支名称，例如 main 或 master
+    "conf_py_path": "./",  # Sphinx 配置文件的路径（相对于仓库根目录）
+}
