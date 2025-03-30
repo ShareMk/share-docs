adb777d490142dc232d9ac10db33aa976f38ab86
ShareMk <33144264+ShareMk@users.noreply.github.com>
2025-03-30 15:05:25 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -25,7 +25,20 @@ extensions = [
 # myst_parser支持 Markdown 中一些扩展语法
 
 myst_enable_extensions = [
-    "highlight"
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
 ]
 
 # 支持的文件扩展名
