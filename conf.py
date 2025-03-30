05e530b3c846c504bc4857e012a78e9971d8327d
ShareMk <33144264+ShareMk@users.noreply.github.com>
2025-03-30 16:24:30 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -51,7 +51,7 @@ pygments_style = 'sphinx'
 #html_static_path = ['_static']
 html_theme = 'sphinx_rtd_theme'
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
+html_static_path = ['docs/_static']
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
